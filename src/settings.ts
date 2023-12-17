9a5a23c96ff1b56992209df465dcf5b0efb754e8
MEKERIAL <mekery00@gmail.com>
2023-12-17 17:23:01 +0300

    add delete all data

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import {blogRoute} from "./routes/blog-route";
 import {postRoute} from "./routes/post-route";
+import {db} from "./db/db";
 
 export const app = express()
 
@@ -8,3 +9,8 @@ app.use(express.json())
 
 app.use('/blogs', blogRoute)
 app.use('/posts', postRoute)
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.blogs.length = 0;
+    db.posts.length = 0;
+    res.sendStatus(204)
+})
