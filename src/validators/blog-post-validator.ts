b3b4b44d94b646e3a8a46ae634ebc681d09186cf
MEKERIAL <mekery00@gmail.com>
2024-01-09 19:40:52 +0500

    add post validation in /:id/posts

--- src/validators/blog-post-validator.ts
+++ src/validators/blog-post-validator.ts
@@ -1,10 +1,9 @@
 import {body, param} from "express-validator"
 import {inputValidation} from "../middlewares/input-model-validation/input-validation";
-import {db} from "../db/db";
 
 export const titleValidation = body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('Incorrect title!')
-export const shortDescriptionValidation = body('description').isString().trim().isLength({min: 1, max: 100}).withMessage('Incorrect shortDescription!')
-export const contentValidation = body('title').isString().trim().isLength({min: 1, max: 1000}).withMessage('Incorrect content!')
+export const shortDescriptionValidation = body('shortDescription').isString().trim().isLength({min: 1, max: 100}).withMessage('Incorrect shortDescription!')
+export const contentValidation = body('content').isString().trim().isLength({min: 1, max: 1000}).withMessage('Incorrect content!')
 
 
 export const blogPostValidation = () => [titleValidation, shortDescriptionValidation, contentValidation, inputValidation]
